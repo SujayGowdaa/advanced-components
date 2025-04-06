524daa868cbc20109a771d0dd6db648e457986cd
SUJAY GOWDA <sujaygowda.dev@gmail.com>
2025-04-06 13:14:01 +0530

    add form custom form and retrive data

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,28 @@
 import { useRef } from 'react';
 import Input from './components/Input';
+import Form from './components/Form';
+import Button from './components/Button';
 
 function App() {
   const inputRef = useRef<HTMLInputElement>(null);
 
+  function handleSave(data: unknown) {
+    const extractedData = data as {
+      name: string;
+      age: string;
+    };
+    console.log(extractedData);
+  }
+
   return (
     <main>
-      <Input label='test' id='test' ref={inputRef} />
+      <Form onSave={handleSave}>
+        <Input label='name' id='name' ref={inputRef} />
+        <Input label='age' id='age' type='number' ref={inputRef} />
+        <p>
+          <Button el='button'>Submit</Button>
+        </p>
+      </Form>
     </main>
   );
 }
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,48 +1,50 @@
 import { type ComponentPropsWithoutRef } from 'react';
 
-// type ButtonProps = {
-//   el: 'button';
-// } & ComponentPropsWithoutRef<'button'>;
+type ButtonProps = {
+  el: 'button';
+} & ComponentPropsWithoutRef<'button'>;
 
-// type AnchorProps = {
-//   el: 'link';
-// } & ComponentPropsWithoutRef<'a'>;
-
-// function Button(props: ButtonProps | AnchorProps) {
-//   if (props.el === 'button') {
-//     return (
-//       <button className='button' {...props}>
-//         A Button
-//       </button>
-//     );
-//   }
-//   if (props.el === 'link') {
-//     return (
-//       <a className='button' {...props}>
-//         A Link
-//       </a>
-//     );
-//   }
-// }
-
-// Another way to render a component without using differentiater prop "el"
-type ButtonProps = ComponentPropsWithoutRef<'button'> & {
-  href?: never;
-};
-
-type AnchorProps = ComponentPropsWithoutRef<'a'> & {
-  href?: string;
-};
-
-function isAnchorProp(props: ButtonProps | AnchorProps): props is AnchorProps {
-  return 'href' in props;
-}
+type AnchorProps = {
+  el: 'link';
+} & ComponentPropsWithoutRef<'a'>;
 
 function Button(props: ButtonProps | AnchorProps) {
-  if (isAnchorProp(props)) {
-    return <a className='button' {...props}></a>;
+  if (props.el === 'button') {
+    return (
+      <button className='button' {...props}>
+        {props.children}
+      </button>
+    );
   }
-  <button className='button' {...props}></button>;
+  if (props.el === 'link') {
+    return (
+      <a className='button' {...props}>
+        A Link
+      </a>
+    );
+  }
 }
+
+// Another way to render a component without using differentiater prop "el"
+// type ButtonProps = ComponentPropsWithoutRef<'button'> & {
+//   href?: never;
+// };
+
+// type AnchorProps = ComponentPropsWithoutRef<'a'> & {
+//   href?: string;
+// };
+
+// function isAnchorProp(props: ButtonProps | AnchorProps): props is AnchorProps {
+//   return 'href' in props;
+// }
+
+// function Button(props: ButtonProps | AnchorProps) {
+//   if (isAnchorProp(props)) {
+//     return <a className='button' {...props}></a>;
+//   }
+//   <button className='button' {...props}>
+//     {props.children}
+//   </button>;
+// }
 
 export default Button;
